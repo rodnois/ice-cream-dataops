cfd71795f2c6502de04d65d0c3e3f02d4f729ab8
dxwf <dxwf@U-3Q46P6CNZN66Q.corp.amazonworkspaces.com>
2024-10-24 10:35:48 -0300

    Configure GitHub action for Data workflows

---
+++ ice-cream-dataops/function_local_venvs/oee_timeseries/run_check.py
@@ -0,0 +1,38 @@
+import os
+from pprint import pprint
+
+from cognite.client import CogniteClient, ClientConfig
+from cognite.client.credentials import OAuthClientCredentials
+
+from code.handler import handle
+
+
+def main() -> None:
+    credentials = OAuthClientCredentials(
+        token_url="https://login.microsoftonline.com/16e3985b-ebe8-4e24-9da4-933e21a9fc81/oauth2/v2.0/token",
+        client_id="bd131bd4-00b7-41ae-99ad-29be4171cd3e",
+        client_secret=os.environ["IDP_CLIENT_SECRET"],
+        scopes=['https://westeurope-1.cognitedata.com/.default'],
+    )
+
+    client = CogniteClient(
+        config=ClientConfig(
+            client_name="CDF-Toolkit:0.3.4",
+            project="cdf-bootcamp-43-test",
+            base_url="https://westeurope-1.cognitedata.com",
+            credentials=credentials,
+        )
+    )
+
+    print("oee_timeseries LOGS:")
+    response = handle(
+        client=client,
+        data={},
+    )
+
+    print("oee_timeseries RESPONSE:")
+    pprint(response)
+
+
+if __name__ == "__main__":
+    main()
